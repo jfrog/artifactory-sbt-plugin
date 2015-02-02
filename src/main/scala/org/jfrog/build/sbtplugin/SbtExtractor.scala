44a7c8b8b696b474807ba6cfe942d3d2c7a07e64
Mark Galpin <markg@jfrog.com>
2015-02-02 11:43:05 -0800

    Added printACC. Added makeACC, which so far just adds the system properties to the ACC as per the Ivy extractor.  Not sure why I'm doing this yet.

--- src/main/scala/org/jfrog/build/sbtplugin/SbtExtractor.scala
+++ src/main/scala/org/jfrog/build/sbtplugin/SbtExtractor.scala
@@ -16,7 +16,10 @@
 package org.jfrog.build.sbtplugin
 
 import org.jfrog.build.client.ArtifactoryClientConfiguration
-import org.jfrog.build.client.DeployDetails  //Provides details for deployment
+import org.jfrog.build.client.DeployDetails
+import org.jfrog.build.extractor.BuildInfoExtractorUtils
+
+//Provides details for deployment
 import org.jfrog.build.api.util.FileChecksumCalculator
 import sbt._
 import org.jfrog.build.api.Module  //Contains build module information
@@ -24,6 +27,7 @@ import org.jfrog.build.api.Module  //Contains build module information
 import java.io.File
 import org.jfrog.build.api.builder.ModuleBuilder
 import org.jfrog.build.extractor.BuildInfoExtractorUtils.getModuleIdString
+import java.util.Properties
 
 /**
  * @author freds
@@ -32,6 +36,7 @@ import org.jfrog.build.extractor.BuildInfoExtractorUtils.getModuleIdString
 object SbtExtractor {
 
   def defineResolvers(resolverConf: ArtifactoryClientConfiguration#ResolverHandler): Seq[Resolver] = {
+    //TODO: I am pretty sure that at this point resolverConf.getUrl is always going to return null, so this function does nothing.  Need to fix?
     val url = resolverConf.getUrl
     import org.apache.commons.lang.StringUtils
     if (StringUtils.isNotBlank(url)) {
@@ -52,7 +57,7 @@ object SbtExtractor {
 
   def extractModule(log: sbt.Logger, artifacts: Map[Artifact, File], report: UpdateReport, moduleId: ModuleID): ArtifactoryModule = {
     log.info(s"BuildInfo: extracting info for module $moduleId")
-  //TODO - figure out what to do with report
+  //TODO - figure out what to do with report, is this the build-info log?
   //  log.info(s"ArtifactoryPluginInfo report: ${report}")
     val module: Module = new ModuleBuilder().id(getModuleIdString(moduleId.organization, moduleId.name, moduleId.revision)).build()
     ArtifactoryModule(module, createDeployDetailsSeq(log, artifacts))
@@ -61,6 +66,7 @@ object SbtExtractor {
   def createDeployDetailsSeq(log: sbt.Logger, artifacts: Map[Artifact, File]): Seq[DeployDetails] = {
     // TODO - Figure out targetRepo
     // TODO - Figure out what to do with extra file metadata.  Properties?
+    // TODO - need to add build info fields, as per buildDeployDetails
     log.info(s"ArtifactoryPluginInfo Artifacts: $artifacts")
     def tempSeqDD: Seq[DeployDetails] = Seq.empty
     if(artifacts.nonEmpty) {
@@ -80,7 +86,116 @@ object SbtExtractor {
 
   def publish(log: sbt.Logger, configuration: ArtifactoryClientConfiguration, modules: Seq[ArtifactoryModule]): Unit = {
     // Publish
+    val myACC = makeACC(log, configuration)
+    printACC(log, myACC)
     log.info(s"BuildInfo: Publishing ${modules.map (_.module.getId) mkString ", "}")
+  }
+
+  def makeACC(log: sbt.Logger, configuration: ArtifactoryClientConfiguration): ArtifactoryClientConfiguration = {
+    val props: Properties = new Properties
+    props.putAll(System.getenv)
+    val myProps = BuildInfoExtractorUtils.mergePropertiesWithSystemAndPropertyFile(props, configuration.getLog)
+    configuration.fillFromProperties(myProps)
+    configuration
+  }
+
+  def printACC(log: sbt.Logger, configuration: ArtifactoryClientConfiguration): Unit = {
+    log.info(s"ArtifactoryClientConfig: $configuration")
+    log.info(s"ArtifactoryClientConfig Base Values:")
+    log.info(s"ACC AllProperties: ${configuration.getAllProperties}")
+    log.info(s"ACC AllRootConfig: ${configuration.getAllRootConfig}")
+    log.info(s"ACC EnvVarsExcludePatterns: ${configuration.getEnvVarsExcludePatterns}")
+    log.info(s"ACC EnvVarsIncludePatterns: ${configuration.getEnvVarsIncludePatterns}")
+    log.info(s"ACC ExportFile: ${configuration.getExportFile}")
+    log.info(s"ACC Log: ${configuration.getLog}")
+    log.info(s"ACC PropertiesFile: ${configuration.getPropertiesFile}")
+    log.info(s"ACC Timeout: ${configuration.getTimeout}")
+    log.info(s"ArtifactoryClientConfig Resolver Values:")
+    log.info(s"ArtifactoryClientConfig resolver: ${configuration.resolver}")
+    log.info(s"ACC Resolver booleans:")
+    log.info(s"ACC Resolver isIvyRepositoryDefined: ${configuration.resolver.isIvyRepositoryDefined}")
+    log.info(s"ACC Resolver isIvy: ${configuration.resolver.isIvy}")
+    log.info(s"ACC Resolver isM2Compatible: ${configuration.resolver.isM2Compatible}")
+    log.info(s"ACC Resolver isMaven: ${configuration.resolver.isMaven}")
+    log.info(s"ACC Resolver values:")
+    log.info(s"ACC Resolver BuildRoot: ${configuration.resolver.getBuildRoot}")
+//    log.info(s"ACC Resolver ContextURL: ${configuration.resolver.getContextUrl}") //TODO: Got a java lang exception that this is undefined is this okay?
+    log.info(s"ACC Resolver DownloadSnapshotRepoKey: ${configuration.resolver.getDownloadSnapshotRepoKey}")
+    log.info(s"ACC Resolver DownloadURL: ${configuration.resolver.getDownloadUrl}")
+    log.info(s"ACC Resolver MatrixParamPrefix: ${configuration.resolver.getMatrixParamPrefix}")
+    log.info(s"ACC Resolver IvyArtifactPattern: ${configuration.resolver.getIvyArtifactPattern}")
+    log.info(s"ACC Resolver IvyPattern: ${configuration.resolver.getIvyPattern}")
+    log.info(s"ACC Resolver Log: ${configuration.resolver.getLog}")
+    log.info(s"ACC Resolver MatrixParams: ${configuration.resolver.getMatrixParams}")
+    log.info(s"ACC Resolver Name: ${configuration.resolver.getName}")
+    log.info(s"ACC Resolver Password: ${configuration.resolver.getPassword}")
+    log.info(s"ACC Resolver Prefix: ${configuration.resolver.getPrefix}")
+    log.info(s"ACC Resolver RepoKey: ${configuration.resolver.getRepoKey}")
+    log.info(s"ACC Resolver URL: ${configuration.resolver.getUrl}")
+    log.info(s"ACC Resolver URLwithMatrixParams: ${configuration.resolver.getUrlWithMatrixParams}")
+    log.info(s"ACC Resolver Username: ${configuration.resolver.getUsername}")
+    log.info(s"ArtifactoryClientConfiguration Publisher publisher Values:")
+    log.info(s"ACC Publisher publisher: ${configuration.publisher}")
+    log.info(s"ACC publisher booleans:")
+    log.info(s"ACC Publisher isCopyAggregatedArtifacts: ${configuration.publisher.isCopyAggregatedArtifacts}")
+    log.info(s"ACC Publisher isEvenUnstable: ${configuration.publisher.isEvenUnstable}")
+    log.info(s"ACC Publisher isFilterExcludedArtifactsFromBuild: ${configuration.publisher.isFilterExcludedArtifactsFromBuild}")
+    log.info(s"ACC Publisher isPublishAggregatedArtifacts: ${configuration.publisher.isPublishAggregatedArtifacts}")
+    log.info(s"ACC Publisher isPublishArtifacts: ${configuration.publisher.isPublishArtifacts}")
+    log.info(s"ACC Publisher isPublishBuildInfo: ${configuration.publisher.isPublishBuildInfo}")
+    log.info(s"ACC Publisher isRecordAllDependencies: ${configuration.publisher.isRecordAllDependencies}")
+    log.info(s"ACC Publisher isIvy: ${configuration.publisher.isIvy}")
+    log.info(s"ACC Publisher isM2Compatible: ${configuration.publisher.isM2Compatible}")
+    log.info(s"ACC Publisher isMaven: ${configuration.publisher.isMaven}")
+    log.info(s"ACC publisher values:")
+    log.info(s"ACC Publisher AggregateArtifacts: ${configuration.publisher.getAggregateArtifacts}")
+    log.info(s"ACC Publisher ArtifactSpecs: ${configuration.publisher.getArtifactSpecs}")
+    log.info(s"ACC Publisher BuildRoot: ${configuration.publisher.getBuildRoot}")
+//    log.info(s"ACC Publisher ContextURL: ${configuration.publisher.getContextUrl}") //TODO: Got a java lang exception that this is undefined is this okay?
+    log.info(s"ACC Publisher getExcludePatterns: ${configuration.publisher.getExcludePatterns}")
+    log.info(s"ACC Publisher getIncludePatterns: ${configuration.publisher.getIncludePatterns}")
+    log.info(s"ACC Publisher MatrixParamPrefix: ${configuration.publisher.getMatrixParamPrefix}")
+    log.info(s"ACC Publisher SnapshotRepoKey: ${configuration.publisher.getSnapshotRepoKey}")
+    log.info(s"ACC Publisher IvyArtifactPattern: ${configuration.publisher.getIvyArtifactPattern}")
+    log.info(s"ACC Publisher IvyPattern: ${configuration.publisher.getIvyPattern}")
+    log.info(s"ACC Publisher Log: ${configuration.publisher.getLog}")
+    log.info(s"ACC Publisher MatrixParams: ${configuration.publisher.getMatrixParams}")
+    log.info(s"ACC Publisher Name: ${configuration.publisher.getName}")
+    log.info(s"ACC Publisher Password: ${configuration.publisher.getPassword}")
+    log.info(s"ACC Publisher Prefix: ${configuration.publisher.getPrefix}")
+    log.info(s"ACC Publisher RepoKey: ${configuration.publisher.getRepoKey}")
+    log.info(s"ACC Publisher URL: ${configuration.publisher.getUrl}")
+    log.info(s"ACC Publisher URLwithMatrixParams: ${configuration.publisher.getUrlWithMatrixParams}")
+    log.info(s"ACC Publisher Username: ${configuration.publisher.getUsername}")
+    log.info(s"ArtifactoryClientConfiguration BuildInfo Stuff:")
+    log.info(s"ArtifactoryClientConfiguration BuildInfo: ${configuration.info}")
+    log.info(s"ACC Info booleans: ")
+    log.info(s"ACC Info isDeleteBuildArtifacts: ${configuration.info.isDeleteBuildArtifacts}")
+    log.info(s"ACC Info isReleaseEnabled: ${configuration.info.isReleaseEnabled}")
+    log.info(s"ACC Info values: ")
+    log.info(s"ACC Info AgentName: ${configuration.info.getAgentName}")
+    log.info(s"ACC Info AgentVersion: ${configuration.info.getAgentVersion}")
+    log.info(s"ACC Info BuildAgentName: ${configuration.info.getBuildAgentName}")
+    log.info(s"ACC Info BuildAgentVersion: ${configuration.info.getBuildAgentVersion}")
+    log.info(s"ACC Info BuildName: ${configuration.info.getBuildName}")
+    log.info(s"ACC Info BuildNumber: ${configuration.info.getBuildNumber}")
+    log.info(s"ACC Info BuildNumbersNotToDelete: ${configuration.info.getBuildNumbersNotToDelete}")
+    log.info(s"ACC Info BuildRetentionCount: ${configuration.info.getBuildRetentionCount}")
+    log.info(s"ACC Info BuildRetentionDays: ${configuration.info.getBuildRetentionDays}")
+    log.info(s"ACC Info BuildRetentionMinimumDate: ${configuration.info.getBuildRetentionMinimumDate}")
+    log.info(s"ACC Info BuildRoot: ${configuration.info.getBuildRoot}")
+    log.info(s"ACC Info BuildStarted: ${configuration.info.getBuildStarted}")
+    log.info(s"ACC Info BuildTimestamp: ${configuration.info.getBuildTimestamp}")
+    log.info(s"ACC Info BuildURL: ${configuration.info.getBuildUrl}")
+    log.info(s"ACC Info ParentBuildName: ${configuration.info.getParentBuildName}")
+    log.info(s"ACC Info ParentBuildNumber: ${configuration.info.getParentBuildNumber}")
+    log.info(s"ACC Info Principal: ${configuration.info.getPrincipal}")
+    log.info(s"ACC Info ReleaseComment: ${configuration.info.getReleaseComment}")
+    log.info(s"ACC Info RunParameters: ${configuration.info.getRunParameters}")
+    log.info(s"ACC Info VcsRevision: ${configuration.info.getVcsRevision}")
+    log.info(s"ACC Info VcsUrl: ${configuration.info.getVcsUrl}")
+    log.info(s"ACC Info Log: ${configuration.info.getLog}")
+    log.info(s"ACC Info Prefix: ${configuration.info.getPrefix}")
   }
 
 }
